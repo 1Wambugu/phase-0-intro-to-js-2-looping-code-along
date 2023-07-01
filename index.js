f452ee763e00a5d39e4d934be5fc9e52926d903c
Martin <martingakuya@gmail.com>
2023-07-01 22:10:46 +0300

    Completed assignmen

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+//function writeCards(names, eventName) {
+  //  const messages = [];
+  
+  //  for (let i = 0; i < names.length; i++) {
+ //     const message = `Thank you, ${names[i]}, for attending the ${eventName}!`;
+  //    messages.push(message);
+ //   }
+  
+  //  return messages;
+ // }
+ // const names = ['Maureen', 'Martin', 'Nathaniel'];
+//const eventName = 'birthday party';
+
+//const result = writeCards(names, eventName);
+//console.log(result);.//
+
+function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  countDown(10);
